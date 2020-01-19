a0999d4e68ed83188d4cbdcc802be0372d37c1a8
Natalia Luzuriaga <natalia.luzuriaga@169-231-114-211.wireless.ucsb.edu>
2020-01-18 16:12:24 -0800

    Added Firebase and MapView

---
+++ ProjectPothole/MapViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MapViewController.swift
+//  ProjectPothole
+//
+//  Created by Natalia Luzuriaga on 1/18/20.
+//  Copyright © 2020 ucsbteam. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+
---
+++ ProjectPothole/StartScreenViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ProjectPothole
+//
+//  Created by Natalia Luzuriaga on 1/18/20.
+//  Copyright © 2020 ucsbteam. All rights reserved.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+
+}
+
