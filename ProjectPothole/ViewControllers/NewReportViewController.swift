6261403cc8919a4b315b28471feb03dff282862e
Natalia Luzuriaga <natalia.luzuriaga@169-231-114-211.wireless.ucsb.edu>
2020-01-19 09:32:28 -0800

    Last minute changes

--- ProjectPothole/ViewControllers/NewReportViewController.swift
+++ ProjectPothole/ViewControllers/NewReportViewController.swift
@@ -1,9 +1,6 @@
 //
-//  ReportViewController.swift
+//  NewReportViewController.swift
 //  ProjectPothole
-//
-//  Created by Natalia Luzuriaga on 1/18/20.
-//  Copyright © 2020 ucsbteam. All rights reserved.
 //
 
 import Foundation
@@ -22,7 +19,7 @@ class NewReportViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         let keyboardDown = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
-           view.addGestureRecognizer(keyboardDown)
+        view.addGestureRecognizer(keyboardDown)
     }
     
     @IBAction func button1Pressed(_ sender: Any) {
@@ -39,44 +36,44 @@ class NewReportViewController: UIViewController{
         severitySelected = 3
         ratingLabel.text = "3"
     }
-
+    
     @IBAction func button4Pressed(_ sender: Any) {
         severitySelected = 4
         ratingLabel.text = "4"
     }
-
+    
     @IBAction func button5Pressed(_ sender: Any) {
         severitySelected = 5
         ratingLabel.text = "5"
     }
-
+    
     @IBAction func submitButtonPressed(_ sender: Any) {
         if (ratingLabel.text == "" || ratingLabel.text == "You must select a number")
-               {
-                   ratingLabel.text = "You must select a number"
-               }
-               else
-               {
-        if let locationSave = ReportLocationViewController.GlobalVariable.saveLocation, let severitySave = severitySelected, let encountersSave = 1 as Double?, let commentSave = textView.text as Any? {
-            db.collection(K.FStore.collectionName).addDocument(data: [
-                K.FStore.locationField: locationSave,
-                K.FStore.severityField: severitySave,
-                K.FStore.encountersField: encountersSave,
-                K.FStore.commentField: FieldValue.arrayUnion([commentSave]),
-            ]) { (error) in
-                if let e = error {
-                    print("There was an issue saving data to firestore, \(e)")
-                } else {
-                    print("Successfully saved data.")
-                    
-                    DispatchQueue.main.async {
-                         self.textView.text = ""
+        {
+            ratingLabel.text = "You must select a number"
+        }
+        else
+        {
+            if let locationSave = ReportLocationViewController.GlobalVariable.saveLocation, let severitySave = severitySelected, let encountersSave = 1 as Double?, let commentSave = textView.text as Any? {
+                db.collection(K.FStore.collectionName).addDocument(data: [
+                    K.FStore.locationField: locationSave,
+                    K.FStore.severityField: severitySave,
+                    K.FStore.encountersField: encountersSave,
+                    K.FStore.commentField: FieldValue.arrayUnion([commentSave]),
+                ]) { (error) in
+                    if let e = error {
+                        print("There was an issue saving data to firestore, \(e)")
+                    } else {
+                        print("Successfully saved data.")
+                        
+                        DispatchQueue.main.async {
+                            self.textView.text = ""
+                        }
+                        UIView.animate(withDuration: 0.1)
+                        {
+                            self.performSegue(withIdentifier: "endingReport", sender: self)
+                        }
                     }
-                    UIView.animate(withDuration: 0.1)
-                    {
-                        self.performSegue(withIdentifier: "endingReport", sender: self)
-                    }
-                }
                 }
             }
         }
--- ProjectPothole/ViewControllers/StartScreenViewController.swift
+++ ProjectPothole/ViewControllers/StartScreenViewController.swift
@@ -1,10 +1,9 @@
-//
-//  ViewController.swift
+
+//  StartScreenViewController.swift
 //  ProjectPothole
 //
-//  Created by Natalia Luzuriaga on 1/18/20.
-//  Copyright © 2020 ucsbteam. All rights reserved.
-//
+
+
 
 import UIKit
 
